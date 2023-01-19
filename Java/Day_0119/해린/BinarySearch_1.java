4d09ba2beb3e617e64619fb30237ecd36c745550
zosunny <104357560+zosunny@users.noreply.github.com>
2023-01-20 01:43:42 +0900

    Update BinarySearch_1.java

--- Java/Day_0119/해린/BinarySearch_1.java
+++ Java/Day_0119/해린/BinarySearch_1.java
@@ -3,38 +3,45 @@ package day0119;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class BJ_1654 {
+public class BJ_1920 {
 	
-	public static void binarySearch_loop(long[] arr, int n) {
-		long lowerBound = 1;
-		long upperBound = arr[arr.length-1];
-		long max = 0;
+	public static void binarySearch_loop(int[] arr, int x) {
+		int arrSize = arr.length;
+		int lowerBound = 0;
+		int upperBound = arrSize - 1;
+		
 		while(lowerBound <= upperBound) {
-			long mid = (upperBound + lowerBound) / 2;
-			long sum = 0;
-			for(int i=0; i<arr.length; i++) {
-				sum += arr[i] / mid;
+			int mid = (upperBound + lowerBound) / 2;
+			
+			if(arr[mid] == x) {
+				System.out.println(1);
+				return;
 			}
-			if(sum < n) {
+			else if(arr[mid] > x) {
 				upperBound = mid - 1;
-			}else {
-				max = mid;
+			}
+			else {
 				lowerBound = mid + 1;
 			}
 		}
-		System.out.println(max);
+		System.out.println(0);
 	}
-
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int K = sc.nextInt();
+		
 		int N = sc.nextInt();
-		long[] line = new long[K];
-		for(int i=0; i<K; i++) {
-			line[i] = sc.nextInt();
+		int[] A = new int[N];
+		for(int i=0; i<N; i++) {
+			A[i] = sc.nextInt();
 		}
-		Arrays.sort(line);
-		binarySearch_loop(line, N);
-		sc.close();
+		
+		Arrays.sort(A);
+		
+		int M = sc.nextInt();
+		for(int i=0; i<M; i++) {
+			int key = sc.nextInt();
+			binarySearch_loop(A, key);
+		}
 	}
 }
